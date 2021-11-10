30ae9c55afdf8500cc8deb6b98bdd78c7690416d
Matheus <mattcontassot@gmail.com>
2021-11-10 19:30:55 -0300

    Exercicio do dia

---
+++ Bloco 7/Dia 1 - JS ES - Arrow functions e etc/parte1.js
@@ -0,0 +1,21 @@
+const testingScope = (escopo) => {
+    if (escopo === true) {
+      var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+      console.log(ifScope);
+    } else {
+      const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
+    console.log(`${ifScope} o que estou fazendo aqui ? :O`); // Se necessário esta linha pode ser removida.
+  }
+
+  testingScope(true);
+
+  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+  oddsAndEvens.sort(function ordem(a, b) {
+      return a - b;
+  })
+
+  console.log(`A ordem correta é ${oddsAndEvens}.`); // será necessário alterar essa linha 😉
